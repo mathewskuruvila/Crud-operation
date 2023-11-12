a306702db87d617ecb8e1650888df0459bb344a4
Mathews <mathewsulike20@gmail.com>
2023-11-12 22:14:08 +0530

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { MdClose } from "react-icons/md";
+import { useState } from "react";
 function App() {
+  const [addSection, setAddSection] = useState(false);
+  const [formData,setFormData] = useState({
+    name :"",
+    email:"",
+    mobile:"",
+  })
+  const handleOnchange = (e)=>{
+    const {value,name} = e.target
+    setFormData((preve)=>{
+      return{
+        ...preve,
+        [name] : value
+      }
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData)
+  };
   return (
     <>
       <div className="container">
-        <button className="btn btn-add">Add</button>
-        <div className="addContainer">
-          <form>
-            <label htmlFor="name">Name :</label>
-            <input type="text" id="name" name="name"/>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email"/>
-            <label htmlFor="mobile">Mobile:</label>
-            <input type="number" id="mobile" name="mobile"/>
-            <button>Submit</button>
-          </form>
-        </div>
+        <button className="btn btn-add" onClick={()=>setAddSection(true)}>Add</button>
+        {addSection && (
+          <div className="addContainer">
+            <form onSubmit={handleSubmit}>
+              <div className="close-btn" onClick={()=>setAddSection(false)} >
+                <MdClose />
+              </div>
+              <label htmlFor="name">Name :</label>
+              <input type="text" id="name" name="name" onChange={handleOnchange}/>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" onChange={handleOnchange} />
+              <label htmlFor="mobile">Mobile:</label>
+              <input type="number" id="mobile" name="mobile" onChange={handleOnchange} />
+              <button className="btn">Submit</button>
+            </form>
+          </div>
+        )}
       </div>
     </>
   );
